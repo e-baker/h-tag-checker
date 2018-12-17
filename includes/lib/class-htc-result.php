9d611884f17ed566356fd6d80f09c3444efd846a
e-baker <eric@trafficlight.me>
2018-12-17 11:59:07 -0500

    Checks for more than one H1 tag

--- includes/lib/class-htc-result.php
+++ includes/lib/class-htc-result.php
@@ -30,6 +30,9 @@ class HTC_Result {
                 $el_output .= "<li class='{$el_type}-result'>" . $el . "</li>";
             }
 
+            if( strtolower($el_type) == 'h1' && count($els) > 1 ) { 
+                $el_output .= "<p class='htc-error-msg'>Your page has more than one <a href='https://trafficlight.me/more-than-one-h1-tag'>H1 tag</a>.</p>";
+            }
             $this->has_duplicates( $els ) ? $el_output .= "<p class='htc-error-msg'>Your page has <a href='https://trafficlight.me/duplicate-{$el_type}-tags'>duplicate {$el_type} tags</a>.</p>" : null;
         
         } else {
