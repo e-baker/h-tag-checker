7880fba860a8c290f082e7084c3e519113bcb1d2
e-baker <eric@trafficlight.me>
2018-12-18 09:53:33 -0500

    Fixes bug when checking for a good url

--- includes/lib/class-htc-api.php
+++ includes/lib/class-htc-api.php
@@ -28,7 +28,7 @@ function good_url( $url ) {
     $http_res = curl_exec( $ch );
     $http_res = trim( strip_tags( $http_res ) );
     $http_code = curl_getinfo( $ch, CURLINFO_HTTP_CODE );
-    $code_type = substr($http_code, 1);
+    $code_type = substr($http_code, 0, 1);
     if(  $code_type === "2" || $code_type === "3" ) {
         return true;
     } else {
